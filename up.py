4c1fde014f2cbf5d8b420506e76f19383e6e00ba
Geo J Vallavancottu <geoj3030@gmail.com>
2023-01-12 21:53:15 +0530

    msql insert

--- up.py
+++ up.py
@@ -2,9 +2,22 @@ import csv
 import json
 import sys
 import requests
-
+import time
+import datetime as dt
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password='',
+    database='voter',
+    auth_plugin='mysql_native_password'
+    )
+vals = []
+sql = "INSERT INTO voters (`name`, `age`, `house_no`, `relation_name`, `part_no`, `serial_no`,`voter_id`) VALUES (%s, %s,%s, %s,%s, %s,%s)"
+mycursor = mydb.cursor()
+start = time.time()
 heads = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-with open('sample1.csv', mode ='r') as file:   
+with open('s1.csv', mode ='r') as file:   
     csvFile = csv.DictReader(file)
     for lines in csvFile:
         print(lines['Part'])
@@ -22,7 +35,17 @@ with open('sample1.csv', mode ='r') as file:
             'voter_id' : lines['EPIC'],
             'ward': lines['Ward'],
         }
-        x = requests.post("https://umapathysrinivasgowda.com/uploadVoterFileApi", data=json.dumps(data), headers=heads)
-        print(x.status_code)
-        print(data);
+        val = (lines['Name'],  lines['Age'],lines['House'],lines['Relative'], lines['Part'], lines['Serial'], lines['EPIC'])
+        vals.append(val)
+        # x = requests.post("http://localhost:8000/uploadVoterFileApi", data=json.dumps(data), headers=heads)
+        # print(x.status_code)
+        print(val);
+
+mycursor.executemany(sql, vals)
+            # print(vals)
+mydb.commit()
+print(mycursor.rowcount, "was inserted.")
+
         # sys.exit()
+end = time.time()
+print("Took {} seconds .".format(end - start))
